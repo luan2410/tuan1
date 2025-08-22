16e570991e3c576a6318f70d0fdbb95567e577b4
H82M19\Student <Student>
2025-08-22 20:22:32 +0700

    bai 3

---
+++ bai3/src/main/java/com/example/bai3/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.example.bai3;
+
+import java.io.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/api/user")
+public class HelloServlet extends HttpServlet {
+   @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = new User(  "nguyen van a", "vana@gmail.com",11);
+
+        //
+       ObjectMapper objectMapper  = new ObjectMapper();
+       String jsonString = objectMapper.writeValueAsString(user);
+
+       response.setContentType("application/json");
+
+       PrintWriter out = response.getWriter();
+       out.println(jsonString);
+       out.flush();
+
+
+
+    }
+
+    public void destroy() {
+    }
+}
